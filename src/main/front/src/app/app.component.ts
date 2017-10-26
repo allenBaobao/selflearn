e7b89a2d2728d93e9ef36def6285961b5dd17e80
Zhou Zhicheng <zhichengzhouenfrance@gmail.com>
2017-10-26 11:26:08 +0200

    d3 js

--- src/main/front/src/app/app.component.ts
+++ src/main/front/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My First Angular App 1';
+
+
+  private chartData: Array<any>;
+
+  constructor() {}
+
+  ngOnInit() {
+    // give everything a chance to get loaded before starting the animation to reduce choppiness
+    setTimeout(() => {
+      this.generateData();
+
+      // change the data periodically
+      setInterval(() => this.generateData(), 3000);
+    }, 1000);
+  }
+
+  generateData() {
+    this.chartData = [];
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
 }
